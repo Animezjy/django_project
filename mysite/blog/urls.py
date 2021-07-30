eaa8e99607bc476deef0bf5fc6e0e883527f8350
zhangjiyou <zhangjiyou@topjoy.com>
2021-07-30 15:16:17 +0800

    初步完成文章页面和详情页

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 app_name = 'blog'
 urlpatterns = [
     path('',views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
