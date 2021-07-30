c8a92d9207225e03a39f9013aa6e61e36a681b19
zhangjiyou <zhangjiyou@topjoy.com>
2021-07-30 11:52:24 +0800

    haha

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -10,13 +10,14 @@ class Post(models.Model):
     slug = models.SlugField(max_length=50, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts", verbose_name="作者")
     status = models.CharField(max_length=20, choices=STATUS_CHOICE, default='draft', verbose_name="文章状态")
+    body = models.TextField(verbose_name="文章内容")
 
     publish = models.DateTimeField(default=timezone.now, verbose_name="发布时间")
     created = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     updated = models.DateTimeField(auto_now=True, verbose_name="更新时间")
 
     class Meta:
-        ordering = "-publish"
+        ordering = ("-publish")
 
     def __str__(self):
         return self.title
