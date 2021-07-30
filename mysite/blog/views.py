3722cab51aa2a0ed588c3d5f023fda38faf7e4c9
zhangjiyou <zhangjiyou@topjoy.com>
2021-07-30 15:35:18 +0800

    使用django自带分页器完成文章分页

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from blog.models import Post
 
 
 def post_list(request):
     posts = Post.published.all()
+    object_list = Post.published.all()  # 筛选所有已发布的文章
+    paginator = Paginator(object_list, 3)  # 每页显示3篇文章
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # 如果page参数不是一个整数就返回第一页
+        posts = paginator.page(1)
+    except EmptyPage:
+        # 如果页数超出总页数就返回最后一页
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', locals())
 
 
