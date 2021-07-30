1f119f8ee29dd2032bfcbbcace67d7e561bc82aa
zhangjiyou <zhangjiyou@topjoy.com>
2021-07-30 14:45:05 +0800

    创建路由和视图函数

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from blog import models
+from blog.models import Post
 # Register your models here.
-admin.site.register(models.Post)
+# admin.site.register(models.Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'created', 'status')
+    list_filter = ('title', 'author', 'publish', 'created', 'status')
+    search_fields = ('title', 'body', 'author')
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('publish', 'status')
+    prepopulated_fields = {'slug': ('title',)}
+
